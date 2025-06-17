489579730e17e44ad6ea0e749ea276f9bdcd85be
Nat <mousseaun@outlook.com>
2025-06-17 16:50:27 -0500

    Added functionality for white bread dough, made layout a bit less ugly

--- main.js
+++ main.js
@@ -1,72 +1,134 @@
 //---PLEASE REFACTOR ALL OF THIS LATER LMAO ITS A MESS
 
-selected_mix = "";
+version_number  = '0.02';
+selected_mix    = '';
 
-//All values in ounces
+//Dictionary storing all values in ounces
 mix_values = {
+
     w_ww_bread : {
-        flour : 704,
-        base : 176,
-        yeast : 20,
-        w_water : 496,
-        ww_water : 512
+        flour       : 704,
+        base        : 176,
+        yeast       : 20,
+        w_water     : 496,
+        ww_water    : 512
+    },
+
+    w_bun_dough : {
+        flour       : 704,
+        base        : 176,
+        yeast       : 20,
+        w_water     : 416
+    },
+
+    ww_bun_dough : {
+        flour       : 704,
+        base        : 176,
+        yeast       : 20,
+        ww_water    : 416
     }
 }
 
 //Fetch document elements
-p_mix_title = document.getElementById('mix_title');
-p_mix_output = document.getElementById('mix_output');
+p_mix_title     = document.getElementById('mix_title');
+p_mix_output    = document.getElementById('mix_output');
 
-inp_mix_input = document.getElementById('mix_input');
+inp_mix_input   = document.getElementById('mix_input');
+inp_hidden      = document.getElementById('hidden_inp');
 
-btn_mix_button = document.getElementById('mix_button');
+btn_mix_button  = document.getElementById('mix_button');
+
+h_h1            = document.getElementById('header');
+
 
 //Initialize mix elements so they start off hidden
 window.addEventListener('DOMContentLoaded', function(event){
-    inp_mix_input.style.display="none";
-    btn_mix_button.style.display="none";
+    h_h1.innerText                  ='Bakery App v' + version_number;
+    inp_mix_input.style.display     ="none";
+    inp_hidden.style.display        ="none";
+    btn_mix_button.style.display    ="none";
 });
 
 //Assign the mix to a value when selected
 function assign_mix(mix) {
+
+    //Assign selected_mix a string value we can use to access dictionary later
     selected_mix = mix;
 
      //Toggle mix visuals
     if(selected_mix == "") {
-        inp_mix_input.style.display="none"
-        btn_mix_button.style.display="none";
+        inp_mix_input.style.display     ="none";
+        inp_hidden.style.display        ="none";
+        btn_mix_button.style.display    ="none";
+    } else {
+        inp_mix_input.style.display     ="block";
+        inp_hidden.style.display        ="block";
+        btn_mix_button.style.display    ="block";
     }
-    else {
-        inp_mix_input.style.display="block"
-        btn_mix_button.style.display="block"
+    
+    //Set title text
+    switch(selected_mix) {
+        case('w_ww_bread'):
+            p_mix_title.innerText = 'White / Whole Wheat Bread'
+            break;
+
+        case('w_bun_dough'):
+            p_mix_title.innerText = 'White Bun Dough'
+            break;
     }
 
+    p_mix_output.innerText = "";
 }
 
 //Calculate value for mix ingredients based off quantity of flour
 function calculate_mix() {
+
+    //Initialize lbs value and factor
+    let lbs_value       = inp_mix_input.value * 16;
+    let factor          = 1;
+
+    //Define temporary variable to store mix data
     let mix = {};
 
     //Grab mix from string
     switch(selected_mix) {
-        case('w-w/w-bread'):
-            mix = mix_values.w_ww_bread;
+
+        case('w_ww_bread'):
+            mix     = mix_values.w_ww_bread;
+            factor  = lbs_value / mix.flour;
+            p_mix_title.innerText = 'White / Whole Wheat Bread'
+            break;
+
+        case('w_bun_dough'):
+            mix     = mix_values.w_bun_dough;
+            factor  = lbs_value / mix.flour;
+            p_mix_title.innerText = 'White Bun Dough'
             break;
     }
     
-    //Converts input lb value into ounces, fetches the mulitplication factor, then converts all values and return as string
-    let lbs_value       = inp_mix_input.value * 16;
-    let factor          = lbs_value / mix.flour;
+    //Calculate individual strings for each ingredient
     let base            = oz_converter(mix.base, factor);
     let yeast           = oz_converter(mix.yeast, factor);
     let white_water     = (mix.w_water  * factor / 16).toString() + " lb";
     let ww_water        = (mix.ww_water * factor / 16).toString() + " lb";
 
-    p_mix_output.innerText = "Base: " + base + "\n" + "Yeast: " + yeast + "\n" + "W Water: " + white_water + "\n" + "W/W Water: " + ww_water;
+    //Create output string to display in browser, maybe make a function to generate string?
+    switch(selected_mix) {
+
+        case('w_ww_bread'):
+            p_mix_output.innerText = "Base: " + base + "\n" + "Yeast: " + yeast + "\n" + "W Water: " + white_water + "\n" + "W/W Water: " + ww_water;
+            break;
+
+        case('w_bun_dough'):
+            p_mix_output.innerText = "Base: " + base + "\n" + "Yeast: " + yeast + "\n" + "Water: " + white_water;
+            break;
+    }
+   
 
 }
 
 //Converts a value in ounces, multiplies it by a factor, and converts it into a string displaying lbs and ounces
+//Maybe make this more legible. Variables name are not good!
 function oz_converter(num, factor) {
     let n = (num * factor) / 16;
     if (n === Math.floor(n)) {
